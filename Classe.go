02f36736b3db775821a42ddd82c3b3d96d3dc065
cauchi <enzocauchi12gmail.com>
2025-09-10 15:07:27 +0200

    creation de perso

---
+++ Classe.go
@@ -0,0 +1,100 @@
+package main
+
+import "fmt"
+
+type personnage struct {
+	nom          string
+	classe       string
+	niveau       int
+	hp           int
+	max_hp       int
+	vitesse      int
+	force        int
+	intelligence int
+	resistance   int
+	chance       int
+	inventaire   []inventaire
+}
+
+func main() {
+	var p personnage
+
+	fmt.Print("Ton blaze: ")
+	fmt.Scan(&p.nom)
+
+	fmt.Println("Ton origine: ")
+	fmt.Println("1. Gitan")
+	fmt.Println("2. Russe")
+	fmt.Println("3. Tchetchene")
+	fmt.Println("4. Malien")
+	fmt.Println("5. Bresilien")
+	fmt.Print("Choix: ")
+
+	var choix int
+	fmt.Scan(&choix)
+
+	switch choix {
+	case 1:
+		p.classe = "Gitan"
+		p.hp = 70
+		p.max_hp = 70
+		p.vitesse = 10
+		p.force = 5
+		p.intelligence = 3
+		p.resistance = 5
+		p.chance = 7
+		p.inventaire = []inventaire{
+			{name: "herisson", quantity: 1},
+		}
+	case 2:
+		p.classe = "Russe"
+		p.hp = 100
+		p.max_hp = 100
+		p.vitesse = 3
+		p.force = 10
+		p.intelligence = 3
+		p.resistance = 7
+		p.chance = 3
+		p.inventaire = []inventaire{
+			{name: "vodka", quantity: 1},
+		}
+	case 3:
+		p.classe = "tchetchene"
+		p.hp = 80
+		p.max_hp = 80
+		p.vitesse = 5
+		p.force = 7
+		p.intelligence = 5
+		p.resistance = 10
+		p.chance = 5
+		p.inventaire = []inventaire{
+			{name: "manuel de soumission", quantity: 1},
+		}
+
+	case 4:
+		p.classe = "Malien"
+		p.hp = 30
+		p.max_hp = 30
+		p.vitesse = 7
+		p.force = 3
+		p.intelligence = 10
+		p.resistance = 3
+		p.chance = 3
+		p.inventaire = []inventaire{
+			{name: "bissap", quantity: 1},
+		}
+	case 5:
+		p.classe = "Bresilien"
+		p.hp = 50
+		p.max_hp = 50
+		p.vitesse = 5
+		p.force = 5
+		p.intelligence = 5
+		p.resistance = 5
+		p.chance = 10
+		p.inventaire = []inventaire{
+			{name: "shamballa", quantity: 1},
+		}
+	}
+	fmt.Printf("Te voila enfin %s le %s\n", p.nom, p.classe)
+}
--- redface.go
+++ redface.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type inventaire struct {
 	name     string
@@ -32,20 +30,5 @@ func Perso() {
 		max_hp: 100,
 		hp:     100,
 	}
-	u.display()
-}
-
-func (u User) display() {
-	fmt.Printf("%s, %d/%d hp, %s, level %d\n", u.name, u.hp, u.max_hp, u.classe, u.level)
-}
-
-func (u *User) make0ld() {
-	u.level += 3
-}
-
-func main() {
-	user := User{"enzo", 4, 'F', "Demon", []inventaire{{"piment", 3}, {"essence", 1}}, 100, 100}
-	user.display()
-	user.make0ld()
-	user.display()
+	fmt.Println(u)
 }
