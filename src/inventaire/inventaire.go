4e9707b612c1346e2e993be0699ab8869d1bf796
fablab12 <fabio.tavolaro@ynov.com>
2025-09-16 15:11:15 +0200

    add

--- src/inventaire/inventaire.go
+++ src/inventaire/inventaire.go
@@ -10,32 +10,32 @@ var MaxSlots = 10
 
 func AfficherSacoche(joueur *class.Personnage) {
     fmt.Println("\n📦 Sacoche :")
-    if len(joueur.Sacoche) == 0 {
+    if len(Sacoche) == 0 {
         fmt.Println("(vide)")
         return
     }
-    for objet, qte := range joueur.Sacoche {
+    for objet, qte := range Sacoche {
         fmt.Printf("- %s : %d\n", objet, qte)
     }
     fmt.Printf("\n🎒 Slots utilisés : %d/%d (reste %d)\n",
-        len(joueur.Sacoche), MaxSlots, MaxSlots-len(joueur.Sacoche))
+        len(Sacoche), MaxSlots, MaxSlots-len(Sacoche))
 }
 
 func AjouterObjet(joueur *class.Personnage, nom string, quantite int) {
-    joueur.Sacoche[nom] += quantite
+    Sacoche[nom] += quantite
     fmt.Printf("✅ %d %s ajouté(s) à la sacoche.\n", quantite, nom)
 }
 
 func UtiliserObjet(nom string, joueur *class.Personnage) {
-    if joueur.Sacoche[nom] > 0 {
+    if Sacoche[nom] > 0 {
         switch nom {
         case "red bull":
             joueur.HP += 20
-            joueur.Sacoche[nom]--
+            Sacoche[nom]--
             fmt.Printf("%s utilise un red bull (+20 PV). PV actuels: %d\n", joueur.Nom, joueur.HP)
         case "Eau":
             joueur.HP += 10
-            joueur.Sacoche[nom]--
+            Sacoche[nom]--
             fmt.Printf("%s utilise une Eau (+10 PV). PV actuels: %d\n", joueur.Nom, joueur.HP)
         default:
             fmt.Println("❌ Objet non utilisable.")
